6eedfe84a68696a7e3cc382933646586acbcc04e
sdhaigthomas <83113978+sdhaigthomas@users.noreply.github.com>
2022-10-18 17:38:04 +0100

    remove calc fetures

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
+
+"""
 from statistics import mode
+from timeit import repeat
 from replit import clear
 from random import randint
 
@@ -7,19 +10,42 @@ class BasicCalc():
     def addition(num1, num2):
         ans = num1 =+ num2
         return ans
+    
+    def subtraction(num1,num2):
+        ans = num1 =- num2
+        return ans
+
+    def multiplication(num1,num2):
+        ans = num1 =- num2
+        return ans
+
+class PrintTG():#tg is for terminal/gui
+    pass
 
 
 def printans(strtp):
-    print("The answer is", str(strtp)+".")
+    print("The answer is", str(strtp) + ".")
 
 #########################
 print("Welcome to Calcpy!")
-mode = input()
+
+#mode = input("Terminal[1], GUI[2]")
+
 
 num1 = 1
 num2 = 3
+
+method = int(input("Addition[1] | Addition[2] | Multiplication[3]"))
+
+
+
+if method == 1: printans(BasicCalc.addition(num1,num2))
+elif method == 2: printans(BasicCalc.subtraction(num1,num2))
 
 printans(BasicCalc.addition(num1,num2))
 
 
 #########################
+
+"""
+
