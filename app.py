dd10db251e93ea9bc22eddc7d59b95d70152e2b6
Sam Haig-Thomas <sam@samscomputer.localdomain>
2022-10-18 19:18:20 +0100

    Changed ;

--- app.py
+++ app.py
@@ -1,51 +1,31 @@
 
-"""
-from statistics import mode
-from timeit import repeat
-from replit import clear
-from random import randint
-
 #########################
-class BasicCalc():
-    def addition(num1, num2):
-        ans = num1 =+ num2
-        return ans
-    
-    def subtraction(num1,num2):
-        ans = num1 =- num2
-        return ans
-
-    def multiplication(num1,num2):
-        ans = num1 =- num2
-        return ans
-
-class PrintTG():#tg is for terminal/gui
-    pass
-
-
-def printans(strtp):
-    print("The answer is", str(strtp) + ".")
-
-#########################
-print("Welcome to Calcpy!")
-
-#mode = input("Terminal[1], GUI[2]")
-
-
-num1 = 1
-num2 = 3
-
-method = int(input("Addition[1] | Addition[2] | Multiplication[3]"))
-
-
-
-if method == 1: printans(BasicCalc.addition(num1,num2))
-elif method == 2: printans(BasicCalc.subtraction(num1,num2))
-
-printans(BasicCalc.addition(num1,num2))
-
+import random
 
 #########################
 
-"""
+print("Calcpy! By sdht")
 
+
+def gtn():
+    while True:
+        turn=0
+        difficulty = input("Easy(1), Medium(2), Hard(3) or Custom(4)\n")
+        if difficulty=="1": amount=25
+        elif difficulty=="2": amount=50
+        elif difficulty=="3": amount=100
+        elif difficulty=="4": amount=int(input("Custom: 1 to\n"))
+        else: print("Unknown mode. Defaulting to 0, 25"), amount
+        ans=random.randint(1,amount)
+        while True:
+            try:geuss=int(input("Whats your guess?\n"))
+            except: geuss=1
+            turn+=1
+            if geuss==ans:
+                print("You won in",turn,"tries!")
+                break
+            else:
+                if geuss<=ans:print("The number is larger then", geuss)
+                else:print("The number is smaller then then", geuss)
+
+gtn()
